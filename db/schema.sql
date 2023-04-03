13c6414c3b0c1ca4d632c669d35f31fc8c841a66
Kameron Hawk <yungkam@Kamerons-MacBook-Pro.local>
2023-04-03 11:58:30 -0700

    auto incrementing tables specified by primary key with foregin key constraints added

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,26 @@
 CREATE DATABASE office_db;
 
-USE  office_db;
+USE office_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL, 
-    department_id INT
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
